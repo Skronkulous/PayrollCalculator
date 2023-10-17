12d08e2634fad789ad0a131e598a656e2f555f7e
Erik Holden <erikholden4@gmail.com>
2023-10-17 13:49:21 -0400

    PayrollCalculator

---
+++ src/main/java/com/pluralsight/Employees.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public class Employees {
+    private int id = 0;
+    private double hoursWorked = 0, payRate = 0;
+    private String name = "";
+
+
+    Employees(int id, String name, double hoursWorked, double payRate) {
+        this.id = id;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getGrossPay(){
+        return payRate*hoursWorked;
+    }
+
+}
---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,73 @@
+package com.pluralsight;
+import java.io.*;
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static Employees[] employeeArray;
+
+    public static void main(String[] args) {
+        csvReader();
+
+    }
+    public static void csvReader(){
+        try {
+            FileReader fr = new FileReader("src/main/resources/employees.csv");
+            String tempInfo = "";
+            int temp = 0;
+            Scanner lengthReader = new Scanner(fr);
+            BufferedReader br = new BufferedReader(fr);
+            br.readLine();
+            employeeArray = new Employees[9];
+            while((tempInfo = br.readLine()) != null){
+                String[] splitLine = tempInfo.split("\\|");
+                   int id = (Integer.parseInt((splitLine[0]).trim()));
+                   String name = (splitLine[1]);
+                   double hoursWorked = (Double.parseDouble((splitLine[2]).trim()));
+                   double payRate = (Double.parseDouble((splitLine[3]).trim()));
+                   Employees newEmployee = new Employees(id, name, hoursWorked, payRate);
+                   employeeArray[temp] = newEmployee;
+                   temp++;
+            }
+        }
+        catch(Exception e){
+            System.out.println("Please fix file path/name. ");
+            csvReader();
+        }
+        displayEmployees();
+    }
+    public static void displayEmployees(){
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Please enter the ID of the employee you would like to see: ");
+            String choice = scan.nextLine();
+            for (Employees employees : employeeArray) {
+                if (Integer.parseInt(choice) == (employees.getId())) {
+                    System.out.println("Employee Id: " + employees.getId() + "\nEmployee Name: " + employees.getName() + "\nEmployee Gross Pay: $" + employees.getGrossPay());
+                    break;
+                }
+            }
+            displayAnother();
+        }
+        catch(Exception f){
+            System.out.println("Please enter a valid ID. ");
+            displayEmployees();
+        }
+    }
+    public static void displayAnother(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Would you like to display another employee? (1 or 2)\n\t1)Yes\n\t2)No\nUser Input: ");
+        String choice = scan.nextLine();
+        switch(Integer.parseInt(choice)){
+            case 1:
+                displayEmployees();
+                break;
+            case 2:
+                System.out.println("See you again soon!");
+                break;
+            default:
+                System.out.println("Please enter a valid input (1 or 2). \n");
+                displayAnother();
+                break;
+        }
+    }
+}
